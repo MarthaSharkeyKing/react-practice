9a75dc65f88b7d1b59bed86cce3daa60c3b0ccb7
bethpritchard <bethpritchard97@gmail.com>
2021-08-18 11:47:54 +0100

    react part of tutorial

--- redux-example/src/App.js
+++ redux-example/src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import logo from "./logo.svg";
+import React, { Component } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import Posts from "./components/Posts";
+import PostForm from "./components/PostForm";
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1> Welcome to React </h1>
+
+        </header>
+        <PostForm /> 
+        <hr />
+        <Posts />
+        <hr />
+        
+      </div>
+    );
+  }
 }
 
 export default App;
